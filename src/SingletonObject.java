d536f5fa9ce3db0b778ca15297c876a1fb68331e
k-minamiguchi <k-minamiguchi@i-enter.co.jp>
2016-12-15 17:36:36 +0900

    Singletonパターンサンプル

---
+++ src/SingletonObject.java
@@ -0,0 +1,9 @@
+public class SingletonObject {
+	private static SingletonObject singletonObject = new SingletonObject();
+
+	private SingletonObject() {}
+
+	public static SingletonObject getInstance() {
+		return singletonObject;
+	}
+}
